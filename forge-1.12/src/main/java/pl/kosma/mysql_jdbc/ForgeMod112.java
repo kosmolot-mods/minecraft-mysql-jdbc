73830929be1c5a406ff7981e208713743397d3b3
Kosma Moczek <kosma@kosma.pl>
2022-09-16 21:56:12 +0200

    fix NoClassDefFoundError not being caught

--- forge-1.12/src/main/java/pl/kosma/mysql_jdbc/ForgeMod112.java
+++ forge-1.12/src/main/java/pl/kosma/mysql_jdbc/ForgeMod112.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 public class ForgeMod112
 {
     public ForgeMod112() {
-        try { new ForgeSetup113(); } catch (Exception e) {}
-        try { new ForgeSetup117(); } catch (Exception e) {}
+        try { new ForgeSetup113(); } catch (Throwable e) {}
+        try { new ForgeSetup117(); } catch (Throwable e) {}
     }
 }
